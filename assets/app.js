8b0b0ab20edb8661846ab3f769bd424f5b9cb485
DiDysh <91142646+DiDysh@users.noreply.github.com>
2022-02-27 17:29:12 +0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+this.pay = function () {
+    var widget = new cp.CloudPayments({language: "en-US"});
+//        widget.pay('auth',
+//            {
+//                publicId: 'pk_d7d3076588bb7b619d4f72d7b21db',
+//                description: document.getElementById('input-description').value,
+//                amount: Number(document.getElementById('input-amount').value),
+//                currency: 'RUB',
+//                email: document.getElementById('input-email').value
+//            },
+//        )  
+//    };
+widget.auth(
+           {
+               publicId: 'pk_d7d3076588bb7b619d4f72d7b21db',
+               description: document.getElementById('input-description').value,
+               amount: "",
+               currency: 'RUB',
+               email: ""
+           },
+       )  
+   };
+
+document.getElementById('form').onsubmit = function (e) {
+    e.preventDefault();
+}
+
+document.getElementById('input-button').onclick = function() {
+    let description = document.getElementById('input-description').value;
+    let email = document.getElementById('input-email').value;
+    let amount = document.getElementById('input-amount').value;
+
+    if ((description.length !=0) && (email.length !=0) && (amount.length !=0)) {
+        pay();
+    }        
+}
+
